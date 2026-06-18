44f97c944c5b700fccdccf1ee33afa73d300d456
Bernd Zeimetz <bernd@bzed.de>
2026-06-18 19:51:52 +0200

    Add GNU AGPL v3 license and copyright headers
    - Add LICENSE file with full GNU Affero General Public License v3 text
    - Add Copyright (C) 2026 Bernd Zeimetz <bernd@bzed.de> to all source files
    - Add AGPL v3 license notice to all Rust source and test files
    
    Generated by Mistral Vibe.
    Co-Authored-By: Mistral Vibe <vibe@mistral.ai>

--- libpve-san/src/lib.rs
+++ libpve-san/src/lib.rs
@@ -5,6 +5,21 @@
 //! - Retrieve disk configurations for each VM
 //! - Discover underlying device-mapper and block devices using lsblk
 //! - Return structured data in a thread-safe manner
+//!
+//! Copyright (C) 2026 Bernd Zeimetz <bernd@bzed.de>
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU Affero General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//! GNU Affero General Public License for more details.
+//!
+//! You should have received a copy of the GNU Affero General Public License
+//! along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use lsblk::BlockDevice;
 use serde::{Deserialize, Serialize};
--- tools/pve-san-query/src/main.rs
+++ tools/pve-san-query/src/main.rs
@@ -3,6 +3,21 @@
 //! This tool retrieves information about running VMs on a Proxmox host,
 //! their configured disks, and underlying block devices.
 //! It can output the data to stdout or a file in JSON format.
+//!
+//! Copyright (C) 2026 Bernd Zeimetz <bernd@bzed.de>
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU Affero General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//! GNU Affero General Public License for more details.
+//!
+//! You should have received a copy of the GNU Affero General Public License
+//! along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use clap::Parser;
 use libpve_san::get_san_storage_info_sync;
